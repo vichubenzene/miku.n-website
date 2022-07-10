1f1c2a608140351deb0224d648942fdc4b060fde
BENZENE <84439064+vichubenzene@users.noreply.github.com>
2022-07-10 17:00:17 +0530

    Update javascript.js

---
+++ javascript.js
@@ -0,0 +1,22 @@
+// var dropdown = document.getElementsByClassName("dropdown-btn");
+// var i;
+
+// for (i = 0; i < dropdown.length; i++) {
+//     dropdown[i].addEventListener("click", function() {
+//         this.classList.toggle("active");
+//         var dropdownContent = this.nextElementSibling;
+//         if (dropdownContent.style.display === "block") {
+//             dropdownContent.style.display = "none";
+//         } else {
+//             dropdownContent.style.display = "block";
+//         }
+//     });
+// }
+
+// function changeImage(hash, imgtxt) {
+//     var image = document.getElementById('myImage');
+//     image.src = `commands/${hash}`;
+//     var imagetxt = document.getElementById('myImagetxt');
+//     imagetxt.textContent = `${imgtxt}`;
+
+// }
